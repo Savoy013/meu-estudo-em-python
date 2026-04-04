6ee925fa6625037d11741260a2e3f698cbc7f677
Savoy013 <pietrocaldeiracampos@gmail.com>
2026-04-04 16:26:50 -0300

    Exercicio 006

---
+++ Exercicios/ex006.py
@@ -0,0 +1,7 @@
+num = int(input("Digite um numero: "))
+raiz_quadrada = num**(1/2)
+dobro = num * 2
+triplo = num * 3
+print(f"Dobro: {dobro}")
+print(f"Tiplo: {triplo}")
+print(f"Raiz Quadrada: {raiz_quadrada:.2f}")
