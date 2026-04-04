c4e0cd2fee7f75abee693eb90a13abdfe124ca6b
Savoy013 <pietrocaldeiracampos@gmail.com>
2026-04-04 16:47:30 -0300

    Exercicio 010!

---
+++ Exercicios/ex010.py
@@ -0,0 +1,12 @@
+"""
+Programa que converte o real para dolar
+"""
+
+# Dolar = 5.16
+
+dolar = 5.16
+real = float(input("Dinheiro em real: "))
+
+conversao = real / dolar
+
+print(f"{real} Reais da no total de {conversao:.2f} Dorales!")
