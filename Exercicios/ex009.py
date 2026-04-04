6100f4be90c4485a8b09fcb2661ec534ad5549a8
Savoy013 <pietrocaldeiracampos@gmail.com>
2026-04-04 16:42:39 -0300

    Exercicio 009

---
+++ Exercicios/ex009.py
@@ -0,0 +1,17 @@
+
+"""
+Programa que ler um numero e mostra
+a Tabuada
+"""
+num = int(input("Numero: "))
+
+print(f"{num} X {1} = {num*1}")
+print(f"{num} X {2} = {num*2}")
+print(f"{num} X {3} = {num*3}")
+print(f"{num} X {4} = {num*4}")
+print(f"{num} X {5} = {num*5}")
+print(f"{num} X {6} = {num*6}")
+print(f"{num} X {7} = {num*7}")
+print(f"{num} X {8} = {num*8}")
+print(f"{num} X {9} = {num*9}")
+print(f"{num} X {10} = {num*10}")
